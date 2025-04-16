27d283f6e6f323c3a14d51a16e4a3fb4be5893a0
rohan <rohanverma031@gmail.com>
2025-04-16 17:12:30 +0530

    feat: added responses used by the app

--- utils/custom/responses.go
+++ utils/custom/responses.go
@@ -1,1 +1,30 @@
-package types
+package custom
+
+// JSONResponse is template for all the api responses
+type JSONResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message,omitempty"`
+	Data    any    `json:"data,omitempty"`
+}
+
+// RequestError implements error interface for request related errors
+type RequestError struct {
+	Status  int // https status code
+	Message string
+}
+
+func (err RequestError) Error() string {
+	return err.Message
+}
+
+// PageInfo defines pagination info for list of data
+type PageInfo struct {
+	NextPage bool   `json:"nextPage"`
+	Cursor   string `json:"cursor"`
+}
+
+// PaginatedData is data form JSONResponse for list of data
+type PaginatedData struct {
+	PageInfo PageInfo `json:"pageInfo"`
+	Items    any      `json:"items"`
+}
