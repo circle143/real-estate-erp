34080f94966ab843b6f1f6ea194946a71355b572
rohan <rohanverma031@gmail.com>
2025-04-16 17:23:11 +0530

    feat: added database migration

--- go.mod
+++ go.mod
@@ -6,3 +6,10 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
 )
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
--- migration/migrate.go
+++ migration/migrate.go
@@ -1,1 +1,24 @@
 package migration
+
+import (
+	"circledigital.in/real-state-erp/models"
+	"gorm.io/gorm"
+	"log"
+)
+
+func migrate(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&models.Organization{},
+		&models.User{},
+		&models.Society{},
+		&models.FlatType{},
+		&models.Tower{},
+		&models.Flat{},
+		&models.Customer{},
+	)
+
+	if err != nil {
+		log.Fatalf("Error migrating db: %v", err)
+	}
+	log.Println("Successfully migrated database models.")
+}
