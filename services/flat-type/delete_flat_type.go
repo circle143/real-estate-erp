f3fc9e19c79d95aef4c7779d253240d35b4e0d9b
rohan <rohanverma031@gmail.com>
2025-04-20 19:03:38 +0530

    fix: fixed flat type delete to handle only my org and society flat type

--- services/flat-type/delete_flat_type.go
+++ services/flat-type/delete_flat_type.go
@@ -12,19 +12,22 @@ import (
 
 type hDeleteFlatType struct{}
 
-func (dft *hDeleteFlatType) execute(db *gorm.DB, flatType string) error {
-	flatTypeModel := models.FlatType{
-		Id: uuid.MustParse(flatType),
-	}
-
-	return db.Delete(&flatTypeModel).Error
+func (dft *hDeleteFlatType) execute(db *gorm.DB, orgId, society, flatType string) error {
+	return db.
+		Model(&models.FlatType{
+			Id: uuid.MustParse(flatType),
+		}).
+		Where("org_id = ? and society_id = ?", orgId, society).
+		Delete(&models.FlatType{}).Error
 }
 
 func (fts *flatTypeService) deleteFlatType(w http.ResponseWriter, r *http.Request) {
 	flatTypeId := chi.URLParam(r, "flatType")
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
 
 	flatType := hDeleteFlatType{}
-	err := flatType.execute(fts.db, flatTypeId)
+	err := flatType.execute(fts.db, orgId, societyRera, flatTypeId)
 	if err != nil {
 		payload.HandleError(w, err)
 		return
