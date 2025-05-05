4bc346039a69f9c4a063daba200e64bce7cef998
rohan <rohanverma031@gmail.com>
2025-05-05 08:30:34 +0530

    feat: added handlers for updating charges price

--- services/charges/patch_charges.go
+++ services/charges/patch_charges.go
@@ -1,13 +1,109 @@
 package charges
 
-import "net/http"
+import (
+	"circledigital.in/real-state-erp/models"
+	"circledigital.in/real-state-erp/utils/common"
+	"circledigital.in/real-state-erp/utils/custom"
+	"circledigital.in/real-state-erp/utils/payload"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type hUpdatePreferenceLocationChargePrice struct {
+	Price float64 `validate:"required"`
+}
+
+func (h *hUpdatePreferenceLocationChargePrice) execute(db *gorm.DB, orgId, society, chargeId string) error {
+	chargeModel := models.PreferenceLocationCharge{
+		Id: uuid.MustParse(chargeId),
+	}
+
+	return db.Transaction(func(tx *gorm.DB) error {
+		// update price in db
+		err := tx.Model(&chargeModel).
+			Where("id = ? AND org_id = ? AND society_id = ?", chargeModel.Id, orgId, society).
+			Update("price", h.Price).Error
+		if err != nil {
+			return err
+		}
+
+		priceHistoryUtil := common.CreatePriceUtil(tx, chargeModel.Id, custom.PREFERENCELOCATIONCHARGE, h.Price)
+		return priceHistoryUtil.AddNewPrice()
+	})
+}
 
 func (s *chargesService) updatePreferenceLocationChargePrice(w http.ResponseWriter, r *http.Request) {
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
+	chargeId := chi.URLParam(r, "chargeId")
+
+	reqBody := payload.ValidateAndDecodeRequest[hUpdatePreferenceLocationChargePrice](w, r)
+	if reqBody == nil {
+		return
+	}
+
+	err := reqBody.execute(s.db, orgId, societyRera, chargeId)
+	if err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+
+	var response custom.JSONResponse
+	response.Error = false
+	response.Message = "Successfully updated price."
+
+	payload.EncodeJSON(w, http.StatusOK, response)
 }
 
 func (s *chargesService) updatePreferenceLocationChargeDetails(w http.ResponseWriter, r *http.Request) {
 }
 
-func (s *chargesService) updateOtherChargePrice(w http.ResponseWriter, r *http.Request) {}
+type hUpdateOtherChargePrice struct {
+	Price float64 `validate:"required"`
+}
+
+func (h *hUpdateOtherChargePrice) execute(db *gorm.DB, orgId, society, chargeId string) error {
+	chargeModel := models.OtherCharge{
+		Id: uuid.MustParse(chargeId),
+	}
+
+	return db.Transaction(func(tx *gorm.DB) error {
+		// update price in db
+		err := tx.Model(&chargeModel).
+			Where("id = ? AND org_id = ? AND society_id = ?", chargeModel.Id, orgId, society).
+			Update("price", h.Price).Error
+		if err != nil {
+			return err
+		}
+
+		priceHistoryUtil := common.CreatePriceUtil(tx, chargeModel.Id, custom.OTHERCHARGE, h.Price)
+		return priceHistoryUtil.AddNewPrice()
+	})
+}
+
+func (s *chargesService) updateOtherChargePrice(w http.ResponseWriter, r *http.Request) {
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
+	chargeId := chi.URLParam(r, "chargeId")
+
+	reqBody := payload.ValidateAndDecodeRequest[hUpdatePreferenceLocationChargePrice](w, r)
+	if reqBody == nil {
+		return
+	}
+
+	err := reqBody.execute(s.db, orgId, societyRera, chargeId)
+	if err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+
+	var response custom.JSONResponse
+	response.Error = false
+	response.Message = "Successfully updated price."
+
+	payload.EncodeJSON(w, http.StatusOK, response)
+}
 
 func (s *chargesService) updateOtherChargeDetails(w http.ResponseWriter, r *http.Request) {}
--- services/flat-type/patch_flat_type.go
+++ services/flat-type/patch_flat_type.go
@@ -15,14 +15,16 @@ type hUpdateFlatType struct {
 	Price float64 `validate:"required"`
 }
 
-func (uft *hUpdateFlatType) execute(db *gorm.DB, flatType string) error {
+func (uft *hUpdateFlatType) execute(db *gorm.DB, orgId, society, flatType string) error {
 	flatTypeModel := models.FlatType{
 		Id: uuid.MustParse(flatType),
 	}
 
 	return db.Transaction(func(tx *gorm.DB) error {
 		// update price in db
-		err := tx.Model(&flatTypeModel).Update("price", uft.Price).Error
+		err := tx.Model(&flatTypeModel).
+			Where("id = ? AND org_id = ? AND society_id = ?", flatTypeModel.Id, orgId, society).
+			Update("price", uft.Price).Error
 		if err != nil {
 			return err
 		}
@@ -33,13 +35,16 @@ func (uft *hUpdateFlatType) execute(db *gorm.DB, flatType string) error {
 }
 
 func (fts *flatTypeService) updateFlatType(w http.ResponseWriter, r *http.Request) {
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
 	flatTypeId := chi.URLParam(r, "flatType")
+
 	reqBody := payload.ValidateAndDecodeRequest[hUpdateFlatType](w, r)
 	if reqBody == nil {
 		return
 	}
 
-	err := reqBody.execute(fts.db, flatTypeId)
+	err := reqBody.execute(fts.db, orgId, societyRera, flatTypeId)
 	if err != nil {
 		payload.HandleError(w, err)
 		return
