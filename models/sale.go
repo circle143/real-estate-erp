ac15f1bfff7d129dd6eae5ef148b7ea2d2a9ef71
rohan <rohanverma031@gmail.com>
2025-05-19 20:30:22 +0530

    update: made company buyer a pointer in sale model

--- models/sale.go
+++ models/sale.go
@@ -24,7 +24,7 @@ type Sale struct {
 	CreatedAt       time.Time             `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt       time.Time             `gorm:"autoUpdateTime" json:"updatedAt"`
 	Customers       []Customer            `gorm:"foreignKey:SaleId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"owners,omitempty"`
-	CompanyCustomer CompanyCustomer       `gorm:"foreignKey:SaleId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"companyCustomer,omitempty"`
+	CompanyCustomer *CompanyCustomer      `gorm:"foreignKey:SaleId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"companyCustomer,omitempty"`
 	//PaymentStatus  []SalePaymentStatus   `gorm:"foreignKey:SaleId" json:"paymentStatus,omitempty"`
 	//DeletedAt      gorm.DeletedAt `gorm:"index"`
 }
