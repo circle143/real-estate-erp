6501df2d0b40de822de126e76eb0132aea68e147
rohan <rohanverma031@gmail.com>
2025-05-20 17:05:40 +0530

    update: added new regex for gst validation from geeksforgeeks

--- utils/validation/gst.go
+++ utils/validation/gst.go
@@ -7,7 +7,7 @@ import (
 
 func CreateGSTValidator() common.IValidator {
 	return &regexValidator{
-		regex:        regexp.MustCompile(`^[0-9]{2}[A-Z]{3}[ABCFGHLJPTF]{1}[A-Z]{1}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$`),
+		regex:        regexp.MustCompile(`^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$`),
 		validatorTag: "gst",
 	}
 }
