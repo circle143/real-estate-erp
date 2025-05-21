3b2b2e8df96c422d7cb7634ad6746eb7152bf319
rohan <rohanverma031@gmail.com>
2025-05-21 17:58:16 +0530

    feat: added handler to get all society brokers

---
+++ services/broker/routes.go
@@ -0,0 +1,31 @@
+package broker
+
+import (
+	"circledigital.in/real-state-erp/utils/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func (s *brokerService) GetBasePath() string {
+	return "/society/{society}/broker"
+}
+
+func (s *brokerService) GetRoutes() *chi.Mux {
+	mux := chi.NewMux()
+	authorizationMiddleware := &middleware.AuthorizationMiddleware{}
+	mux.Group(func(router chi.Router) {
+		router.Use(authorizationMiddleware.OrganizationAdminAuthorization)
+		router.Use(authorizationMiddleware.OrganizationAuthorization)
+
+		router.Post("/", s.addBrokerToSociety)
+		router.Patch("/{brokerId}", s.updateBrokerDetails)
+	})
+
+	mux.Group(func(router chi.Router) {
+		router.Use(authorizationMiddleware.OrganizationAdminAndUserAuthorization)
+		router.Use(authorizationMiddleware.OrganizationAuthorization)
+
+		router.Get("/", s.getAllSocietyBrokers)
+	})
+
+	return mux
+}
