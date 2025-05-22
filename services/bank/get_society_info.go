5c3f82adedc9137acb6bac145621a47b96bb48be
rohan <rohanverma031@gmail.com>
2025-05-22 16:16:51 +0530

    feat: added bank society info service

--- services/bank/get_society_info.go
+++ services/bank/get_society_info.go
@@ -1,1 +1,37 @@
 package bank
+
+import (
+	"circledigital.in/real-state-erp/models"
+	"circledigital.in/real-state-erp/utils/common"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type bankSocietyInfoService struct {
+	db     *gorm.DB
+	bankId uuid.UUID
+}
+
+func (s *bankSocietyInfoService) GetSocietyInfo() (*common.SocietyInfo, error) {
+	// fetch from db and return
+	bank := models.Bank{
+		Id: s.bankId,
+	}
+
+	err := s.db.First(&bank).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &common.SocietyInfo{
+		OrgId:       bank.OrgId,
+		SocietyRera: bank.SocietyId,
+	}, nil
+}
+
+func CreateBankSocietyInfoService(db *gorm.DB, bankId uuid.UUID) common.ISocietyInfo {
+	return &bankSocietyInfoService{
+		db:     db,
+		bankId: bankId,
+	}
+}
