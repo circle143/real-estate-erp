01c94787123587c3e370f119e313251df436bb9f
rohan <rohanverma031@gmail.com>
2025-05-22 16:18:15 +0530

    feat: added handler to get all society bank account details

--- services/bank/get_bank.go
+++ services/bank/get_bank.go
@@ -1,5 +1,51 @@
 package bank
 
-import "net/http"
+import (
+	"circledigital.in/real-state-erp/models"
+	"circledigital.in/real-state-erp/utils/common"
+	"circledigital.in/real-state-erp/utils/custom"
+	"circledigital.in/real-state-erp/utils/payload"
+	"github.com/go-chi/chi/v5"
+	"gorm.io/gorm"
+	"net/http"
+	"strings"
+)
 
-func (s *bankService) getAllSocietyBankAccounts(w http.ResponseWriter, r *http.Request) {}
+type hGetAllSocietyBankAccounts struct{}
+
+func (h *hGetAllSocietyBankAccounts) execute(db *gorm.DB, orgId, society, cursor string) (*custom.PaginatedData, error) {
+	var bankAccountData []models.Bank
+
+	query := db.Where("org_id = ? and society_id = ?", orgId, society).Order("created_at DESC").Limit(custom.LIMIT + 1)
+	if strings.TrimSpace(cursor) != "" {
+		decodedCursor, err := common.DecodeCursor(cursor)
+		if err == nil {
+			query = query.Where("created_at < ?", decodedCursor)
+		}
+	}
+
+	err := query.Find(&bankAccountData).Error
+	if err != nil {
+		return nil, err
+	}
+	return common.CreatePaginatedResponse(&bankAccountData), nil
+}
+
+func (s *bankService) getAllSocietyBankAccounts(w http.ResponseWriter, r *http.Request) {
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	cursor := r.URL.Query().Get("cursor")
+	societyRera := chi.URLParam(r, "society")
+
+	bank := hGetAllSocietyBankAccounts{}
+	res, err := bank.execute(s.db, orgId, societyRera, cursor)
+	if err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+
+	var response custom.JSONResponse
+	response.Error = false
+	response.Data = res
+
+	payload.EncodeJSON(w, http.StatusOK, response)
+}
