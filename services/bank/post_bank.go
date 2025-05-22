c4294c4154d21aa989412758ff3869c725f83dcb
rohan <rohanverma031@gmail.com>
2025-05-22 16:22:33 +0530

    feat: added handler for adding a bank account to society

--- services/bank/post_bank.go
+++ services/bank/post_bank.go
@@ -1,5 +1,51 @@
 package bank
 
-import "net/http"
+import (
+	"circledigital.in/real-state-erp/models"
+	"circledigital.in/real-state-erp/utils/custom"
+	"circledigital.in/real-state-erp/utils/payload"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"net/http"
+)
 
-func (s *bankService) addBankAccountToSociety(w http.ResponseWriter, r *http.Request) {}
+type hAddBankAccountToSociety struct {
+	Name          string `validate:"required"`
+	AccountNumber string `validate:"required,bank-account-number"`
+}
+
+func (h *hAddBankAccountToSociety) execute(db *gorm.DB, orgId, society string) (*models.Bank, error) {
+	bankModel := models.Bank{
+		OrgId:         uuid.MustParse(orgId),
+		SocietyId:     society,
+		Name:          h.Name,
+		AccountNumber: h.AccountNumber,
+	}
+
+	err := db.Create(&bankModel).Error
+	return &bankModel, err
+}
+
+func (s *bankService) addBankAccountToSociety(w http.ResponseWriter, r *http.Request) {
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
+
+	reqBody := payload.ValidateAndDecodeRequest[hAddBankAccountToSociety](w, r)
+	if reqBody == nil {
+		return
+	}
+
+	bank, err := reqBody.execute(s.db, orgId, societyRera)
+	if err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+
+	var response custom.JSONResponse
+	response.Error = false
+	response.Message = "Successfully added bank account to society."
+	response.Data = bank
+
+	payload.EncodeJSON(w, http.StatusCreated, response)
+}
