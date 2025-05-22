cc7274fae09425c31a6e791a0f7fb3dc2ca81ec7
rohan <rohanverma031@gmail.com>
2025-05-22 16:10:05 +0530

    update: added org id to broker aadhar and pan unique index

--- models/broker_model.go
+++ models/broker_model.go
@@ -8,7 +8,7 @@ import (
 type Broker struct {
 	Id           uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
 	SocietyId    string    `gorm:"not null;index;uniqueIndex:idx_society_aadhar;uniqueIndex:idx_society_pan" json:"societyId"`
-	OrgId        uuid.UUID `gorm:"not null;index" json:"orgId"`
+	OrgId        uuid.UUID `gorm:"not null;index;uniqueIndex:idx_society_aadhar;uniqueIndex:idx_society_pan" json:"orgId"`
 	Society      *Society  `gorm:"foreignKey:SocietyId,OrgId;references:ReraNumber,OrgId;not null;constraint:OnUpdate:CASCADE" json:"society,omitempty"`
 	Name         string    `gorm:"not null" json:"name"`
 	AadharNumber string    `gorm:"not null;uniqueIndex:idx_society_aadhar" json:"aadharNumber"`
