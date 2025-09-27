7ba010080a779774018ae5ac1ce85f8017f2925b
rohan <rohanverma031@gmail.com>
2025-09-27 15:14:30 +0530

    feat: added master report impl

--- go.mod
+++ go.mod
@@ -12,10 +12,12 @@ require (
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.5.5
 	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
 	github.com/shopspring/decimal v1.4.0
+	github.com/thedatashed/xlsxreader v1.2.8
 	github.com/vmihailenco/msgpack/v5 v5.4.1
+	github.com/xuri/excelize/v2 v2.9.1
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
@@ -38,17 +40,20 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/thedatashed/xlsxreader v1.2.8 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/tiendc/go-deepcopy v1.6.0 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	github.com/xuri/efp v0.0.1 // indirect
+	github.com/xuri/nfp v0.0.1 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/time v0.9.0 // indirect
 )
--- services/reports/master_reports.go
+++ services/reports/master_reports.go
@@ -1,7 +1,238 @@
 package reports
 
-import "net/http"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+
+	"circledigital.in/real-state-erp/models"
+	"circledigital.in/real-state-erp/utils/custom"
+	"circledigital.in/real-state-erp/utils/payload"
+	"github.com/go-chi/chi/v5"
+	"github.com/xuri/excelize/v2"
+	"gorm.io/gorm"
+)
+
+func newMasterReportSheet(file *excelize.File, tower models.Tower) error {
+	sheetName := tower.Name
+	_, err := file.NewSheet(sheetName)
+	if err != nil {
+		return err
+	}
+
+	// Header row (UUID + time fields removed)
+	headers := []string{
+		"Tower_Name", "Flat_Name", "FloorNumber", "Facing", "SaleableArea", "UnitType",
+		"SaleNumber", "TotalPrice",
+		"BrokerName", "BrokerAadhar", "BrokerPan",
+		"PaymentPlanGroup", "PaymentPlanRatio",
+		"CustomerName", "CustomerEmail", "CustomerPhone", "CustomerPan", "CustomerAadhar",
+		"CompanyName", "CompanyPan", "CompanyGst",
+	}
+	for i, h := range headers {
+		cell, _ := excelize.CoordinatesToCellName(i+1, 1)
+		if err := file.SetCellValue(sheetName, cell, h); err != nil {
+			return err
+		}
+	}
+
+	// Data rows
+	rowIdx := 2
+	for _, flat := range tower.Flats {
+		baseFlat := []any{
+			sheetName,
+			flat.Name,
+			flat.FloorNumber,
+			flat.Facing,
+			flat.SaleableArea.String(),
+			flat.UnitType,
+		}
+
+		if flat.SaleDetail != nil {
+			sale := flat.SaleDetail
+
+			// sale values (removed ID + timestamps)
+			saleVals := []any{
+				sale.SaleNumber,
+				sale.TotalPrice.String(),
+			}
+
+			// broker values (removed ID)
+			var brokerVals []any
+			if sale.Broker != nil {
+				brokerVals = []any{
+					sale.Broker.Name,
+					sale.Broker.AadharNumber,
+					sale.Broker.PanNumber,
+				}
+			} else {
+				brokerVals = []any{"-", "-", "-"}
+			}
+
+			// payment plan group + ratio
+			var planVals []any
+			if sale.PaymentPlanRatio != nil && sale.PaymentPlanRatio.PaymentPlanGroup != nil {
+				planVals = []any{
+					sale.PaymentPlanRatio.PaymentPlanGroup.Name,
+					sale.PaymentPlanRatio.Ratio,
+				}
+			} else {
+				planVals = []any{"-", "-"}
+			}
+
+			// customers
+			if len(sale.Customers) > 0 {
+				for _, cust := range sale.Customers {
+					custVals := []any{
+						fmt.Sprintf("%s %s %s", cust.FirstName, cust.MiddleName, cust.LastName),
+						cust.Email,
+						cust.PhoneNumber,
+						cust.PanNumber,
+						cust.AadharNumber,
+					}
+					companyVals := []any{"-", "-", "-"}
+					rowVals := append(append(append(append(baseFlat, saleVals...), brokerVals...), planVals...), custVals...)
+					rowVals = append(rowVals, companyVals...)
+					for colIdx, val := range rowVals {
+						cell, _ := excelize.CoordinatesToCellName(colIdx+1, rowIdx)
+						if err := file.SetCellValue(sheetName, cell, val); err != nil {
+							return err
+						}
+					}
+					rowIdx++
+				}
+			} else if sale.CompanyCustomer != nil {
+				// company customer row (removed ID)
+				custVals := []any{"-", "-", "-", "-", "-"}
+				companyVals := []any{
+					sale.CompanyCustomer.Name,
+					sale.CompanyCustomer.CompanyPan,
+					sale.CompanyCustomer.CompanyGst,
+				}
+				rowVals := append(append(append(append(baseFlat, saleVals...), brokerVals...), planVals...), custVals...)
+				rowVals = append(rowVals, companyVals...)
+				for colIdx, val := range rowVals {
+					cell, _ := excelize.CoordinatesToCellName(colIdx+1, rowIdx)
+					if err := file.SetCellValue(sheetName, cell, val); err != nil {
+						return err
+					}
+				}
+				rowIdx++
+			} else {
+				// sale without customer
+				custVals := []any{"-", "-", "-", "-", "-"}
+				companyVals := []any{"-", "-", "-"}
+				rowVals := append(append(append(append(baseFlat, saleVals...), brokerVals...), planVals...), custVals...)
+				rowVals = append(rowVals, companyVals...)
+				for colIdx, val := range rowVals {
+					cell, _ := excelize.CoordinatesToCellName(colIdx+1, rowIdx)
+					if err := file.SetCellValue(sheetName, cell, val); err != nil {
+						return err
+					}
+				}
+				rowIdx++
+			}
+		} else {
+			// flat without sale
+			emptySale := []any{"-", "-"}
+			emptyBroker := []any{"-", "-", "-"}
+			emptyPlan := []any{"-", "-"}
+			emptyCust := []any{"-", "-", "-", "-", "-"}
+			emptyCompany := []any{"-", "-", "-"}
+			rowVals := append(append(append(append(baseFlat, emptySale...), emptyBroker...), emptyPlan...), emptyCust...)
+			rowVals = append(rowVals, emptyCompany...)
+			for colIdx, val := range rowVals {
+				cell, _ := excelize.CoordinatesToCellName(colIdx+1, rowIdx)
+				if err := file.SetCellValue(sheetName, cell, val); err != nil {
+					return err
+				}
+			}
+			rowIdx++
+		}
+	}
+
+	return nil
+}
+
+func generateMasterReport(db *gorm.DB, orgId, society string) (*bytes.Buffer, error) {
+	var towerData []models.Tower
+	err := db.
+		Where("org_id = ? AND society_id = ?", orgId, society).
+		Preload("Flats").
+		Preload("Flats.SaleDetail").
+		Preload("Flats.SaleDetail.PaymentPlanRatio").
+		Preload("Flats.SaleDetail.PaymentPlanRatio.PaymentPlanGroup").
+		Preload("Flats.SaleDetail.Receipts").
+		Preload("Flats.SaleDetail.Receipts.Cleared").
+		Preload("Flats.SaleDetail.Broker").
+		Preload("Flats.SaleDetail.Customers").
+		Preload("Flats.SaleDetail.CompanyCustomer").
+		Find(&towerData).Error
+	if err != nil {
+		return nil, err
+	}
+
+	if len(towerData) < 1 {
+		return nil, &custom.RequestError{
+			Status:  http.StatusNoContent,
+			Message: "No tower found",
+		}
+	}
+
+	var reportFile *excelize.File = nil
+	if os.Getenv("ENV") == "development" {
+		reportFile = excelize.NewFile()
+	} else {
+		reportFile = excelize.NewFile(
+			excelize.Options{
+				Password: society,
+			},
+		)
+	}
+
+	for _, tower := range towerData {
+		sheetErr := newMasterReportSheet(reportFile, tower)
+		if sheetErr != nil {
+			return nil, sheetErr
+		}
+	}
+
+	// Delete the default "Sheet1"
+	if err := reportFile.DeleteSheet("Sheet1"); err != nil {
+		return nil, err
+	}
+
+	// Write Excel file to buffer
+	var buf bytes.Buffer
+	if err := reportFile.Write(&buf); err != nil {
+		return nil, err
+	}
+	return &buf, nil
+}
 
 // generateMasterReport() generates a master report
 // master report contains all the flats and its related sale details
-func (s *reportService) generateMasterReport(w http.ResponseWriter, r *http.Request) {}
+func (s *reportService) generateMasterReport(w http.ResponseWriter, r *http.Request) {
+	// get society rera and org id from request
+	orgId := r.Context().Value(custom.OrganizationIDKey).(string)
+	societyRera := chi.URLParam(r, "society")
+
+	report, err := generateMasterReport(s.db, orgId, societyRera)
+	if err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+
+	// Set headers so browser/download tools recognize it as Excel
+	w.Header().Set("Content-Type",
+		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s_master_report.xlsx", societyRera))
+	w.Header().Set("Content-Length", fmt.Sprint(report.Len()))
+
+	// Write to response
+	if _, err := w.Write(report.Bytes()); err != nil {
+		payload.HandleError(w, err)
+		return
+	}
+}
